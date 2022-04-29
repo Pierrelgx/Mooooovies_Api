1041cbda223dfd4c5a394798cd67cabe68dacd42
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-04-29 08:07:47 +0200

    first

--- script.js
+++ script.js
@@ -3,28 +3,30 @@ const btn = document.querySelector('.btn');
 const xhr = new XMLHttpRequest();
 
 btn.addEventListener('click', () => {
-    console.log("Hello les coupaings");
-})
+    
 
 
 
-xhr.open("GET", "http://www.omdbapi.com/?i=tt3896198&apikey=a018a06a");
-xhr.responseType = "json" ; 
-console.log(xhr.response);
-
-xhr.onload = function(){
+    xhr.open("GET", "http://www.omdbapi.com/?i=tt3896198&apikey=a018a06a");
+    xhr.responseType = "json" ; 
     console.log(xhr.response);
-    for (i = 0; i < xhr.lengts; i ++) {
-        
-        let newLi = document.createElement('li');
-        let newTitreCarte = document.createElement('h2');
-        let newBodyCarte = document.createElement('p');
 
-        newTitreCarte.innerText = xhr.response[i].title;
-        newBodyCarte.innerText = xhr.response[i].body;
-        
+    xhr.onload = function(){
+        console.log(xhr.response);
+
+        for (i = 0; i < xhr.response.lengts; i ++) {
+            
+            let newLi = document.createElement('li');
+            let newTitreCarte = document.createElement('h2');
+            let newBodyCarte = document.createElement('p');
+
+            newTitreCarte.innerText = xhr.response[i].title;
+            newBodyCarte.innerText = xhr.response[i].body;
+            
+        }
+
     }
 
-}
+})
 
 xhr.send();
